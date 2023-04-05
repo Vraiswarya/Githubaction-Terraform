44b79fbed87f21d98031957d1886eb7ac0cd23de
Aiswarya V R <aiswarya.v.r@inter.ikea.com>
2023-04-05 16:24:48 +0200

    Terraform-demo01

--- aishu/backend.tf
+++ aishu/backend.tf
@@ -1,5 +1,8 @@
-resource_group_name  ="rg_test1"
-storage_account_name ="sdatatest01"
-container_name       ="myterraformstate"
-key                  ="tfstatefile.json"
-
+terraform{
+    backend "azurerm"{
+        resource_group_name ="rg_test1"
+        storage_account_name="sdatatest01"
+        container_name="myterraformstate"
+        key="tfstatefile.json"
+    }
+}
