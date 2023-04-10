7e5653931b78e5800aa341194599b09fb44a8f35
Aiswarya V R <aiswarya.v.r@inter.ikea.com>
2023-04-10 17:11:29 +0200

    Terraform-demo01

--- aishu/APG/main.tf
+++ aishu/APG/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_application_gateway" "APG" {
 
   gateway_ip_configuration {
     name      = "dqgatewayipcon"
-    subnet_id = "10.103.70.165/28"
+    subnet_id = ["10.103.70.165/28"]
   }
 
   frontend_port {
