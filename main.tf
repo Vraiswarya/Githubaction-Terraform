6fa7627c6481e77db6b120206c275cc3503f0b6e
Aiswarya V R <aiswarya.v.r@inter.ikea.com>
2023-04-10 16:48:11 +0200

    Terraform-demo01

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ provider "azurerm" {
 module "APG" {
   source   = "./aishu/APG"
   apgname    = var.apgname
-  rgname   = var.rg_name
+  rg_name   = var.rg_name
   location = var.location
 }
 module "RG" {
     source   = "./aishu/RG" #A 
-    rgname   = var.rg_name     #B 
+    rg_name   = var.rg_name     #B 
     location = var.location
 }
 module "ACR" {
     source   = "./aishu/ACR" #A 
-    rgname   = var.rg_name     #B 
+    rg_name  = var.rg_name     #B 
     location = var.location
 }
