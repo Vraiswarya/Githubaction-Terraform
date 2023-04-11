97e675b026195908ac068f6ae50e0de05b58da5b
Aiswarya V R <aiswarya.v.r@inter.ikea.com>
2023-04-11 07:51:53 +0200

    Terraform-demo01

--- aishu/APG/main.tf
+++ aishu/APG/main.tf
@@ -1,5 +1,15 @@
 
+data "azurerm_virtual_network" "vnet_resource" {
+  name                = "vnet-dq-test-euwe-002"
+  resource_group_name =  var.rg_name
+}
 
+data "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = var.rg_name
+  virtual_network_name = data.azurerm_virtual_network.vnet_resource.name
+ 
+}
 resource "azurerm_application_gateway" "APG" {
   name                = var.apgname
   resource_group_name = var.rg_name
@@ -13,7 +23,7 @@ resource "azurerm_application_gateway" "APG" {
 
   gateway_ip_configuration {
     name      = "dqgatewayipcon"
-    subnet_id = "10.103.70.165/28"
+    subnet_id = data.azurerm_subnet.frontend.id
   }
 
   frontend_port {
